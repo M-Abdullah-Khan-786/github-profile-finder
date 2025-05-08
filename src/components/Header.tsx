73a1793bbfa7b6ded01d743c3238852e4749a064
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-05-08 19:12:04 +0500

    Create Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 text-center text-2xl font-bold shadow-md">
+      GitHub Profile Finder 🔍
+    </header>
+  );
+};
+
+export default Header;
