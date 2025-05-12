0bc21c5a58b6107b2a3be97d7dd96d497a3a5176
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-05-12 18:11:30 +0500

    Integrate the Components in Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
 import Header from "../components/Header";
+import SearchBar from "../components/FindUser";
+import UserCard from "../components/Card";
+import RepoList from "../components/RepoList";
 
 export default function HomePage() {
+  const [user, setUser] = useState<any>(null);
+  const [repos, setRepos] = useState<any[]>([]);
+  const [error, setError] = useState<string>("");
 
+  const handleSearch = async (username: string) => {
+    console.log("Search User");
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <Header />
+      <SearchBar onSearch={handleSearch} />
+      {error && <p className="text-center text-red-500 mt-4">{error}</p>}
+      {user && <UserCard user={user} />}
+      {repos.length > 0 && <RepoList repos={repos} />}
     </div>
   );
 }
